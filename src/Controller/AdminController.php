adde6bd6d9152652f9cdc939e1e4a43eccb4b7f7
Roger-45130 <roger-lg@france-usa-net.com>
2025-03-10 13:03:39 +0100

    Mise a Jour du code

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -20,6 +20,12 @@ final class AdminController extends AbstractController
         return $this->render('admin/products.html.twig', []);
     }
 
+    #[Route('/admin/category', name: 'app_admin_category')]
+    public function adminCategory(): Response
+    {
+        return $this->render('admin/category.html.twig', []);
+    }
+
     #[Route('/admin/orders', name: 'app_admin_orders')]
     public function adminOrders(): Response
     {
